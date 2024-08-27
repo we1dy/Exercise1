263a37ed0465be90f0107293ad16a87612340857
we1dy <ladyarboleda26@gmail.com>
2024-08-28 01:05:16 +0800

    Add project files.

---
+++ Exercise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Exercise1
+{
+	public static void Main()
+	{
+		char letter, letter1, letter2;
+
+		Console.Write("Input letter: ");
+		letter = Convert.ToChar(Console.ReadLine());
+
+		Console.Write("Input letter: ");
+		letter1 = Convert.ToChar(Console.ReadLine());
+
+		Console.Write("Input letter: ");
+		letter2 = Convert.ToChar(Console.ReadLine());
+
+		Console.WriteLine("{0} {1} {2}", letter2, letter1, letter);
+	}
+}
